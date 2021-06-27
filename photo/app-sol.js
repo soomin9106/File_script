d9dc6a790cdda308ea227e469e7297d2ea85136c
soomin9106 <79344555+soomin9106@users.noreply.github.com>
2021-06-27 20:46:10 +0900

    solution code

--- photo/app-sol.js
+++ photo/app-sol.js
@@ -2,12 +2,12 @@ const path= require('path');
 const fs=require('fs');
 const os= require('os');
 
-
-// 계획
-// 1. 사용자가 원하는 폴더의 이름을 받아온다
-const folder = process.argv[2];
+//1. 사용자가 원하는 폴더의 이름을 받음
+//실행 할 때 : nodemon app-sol test 이니까 두번째 인자 받아오면 됨.
+const folder=process.argv[2]; //test 를 받아옴
 const workingDir= path.join(os.homedir(),'Desktop','Pictures',folder);
-
+console.log(folder);
+console.log(workingDir);
 if(!folder || !fs.existsSync(workingDir)){
     console.log('There are no such folder or directory!');
     return ;
@@ -22,7 +22,6 @@ const duplicatedDir=path.join(workingDir,'duplicated');
 !fs.existsSync(capturedDir) && fs.mkdirSync(capturedDir);
 !fs.existsSync(duplicatedDir) && fs.mkdirSync(duplicatedDir);
 
-
 // 3. 폴더안에 있는 파일들을 다 돌면서 해당하는 mp4|mov, png|aae, IMG_1234 (IMG_E1234)
 fs.promises
 .readdir(workingDir)
@@ -30,43 +29,38 @@ fs.promises
 .catch(console.log);
 
 function processFiles(files){
-    files.forEach((file) => {
-       if(isVideoFile(file)){
-        move(file,videoDir);
-       } else if(isCapturedFile(file)){
-           move(file,capturedDir);
-       } else if(isDuplicatedFile(file)){
-           move(file,duplicatedDir);
-       }
+    files.forEach((file)=>{
+        if(isVideoFile(file)){
+            move(file,videoDir);
+           } else if(isCapturedFile(file)){
+               move(file,capturedDir);
+           } else if(isDuplicatedFile(files,file)){
+               move(file,duplicatedDir);
+           }
     });
+
 }
 
 function isVideoFile(file){
-    ext=path.extname(file); //파일의 확장자
-    if(ext ==='.mp4' || ext ==='.mov'){
-        return 1;
-    } else{
-        return -1;
-    }
+   const regExp= /(mp4|mov)$/gm;
+   const match=file.match(regExp);
+   return !!match;
 }
 
 function isCapturedFile(file){
-    ext=path.extname(file); //파일의 확장자
-    if(ext ==='.png' || ext ==='.aae'){
-        return 1;
-    } else{
-        return -1;
-    }
+    const regExp= /(png|aae)$/gm;
+    const match=file.match(regExp);
+    return !!match;
 }
 
-function isDuplicatedFile(file){
-    if(!file.startsWith('IMG_') || file.startsWith('IMG_E')){
-        return false;
-    } 
- 
-    const edited=`IMG_E${file.split('_')[1]}`;
-    const found=files.find(f => f.includes(edited));
-    return !!found;
+function isDuplicatedFile(files,file){
+   if(!file.startsWith('IMG_') || file.startsWith('IMG_E')){
+       return false;
+   } 
+
+   const edited=`IMG_E${file.split('_')[1]}`;
+   const found=files.find(f => f.includes(edited));
+   return !!found;
 }
 
 function move(file,dir){
@@ -75,6 +69,5 @@ function move(file,dir){
     fs.promises
     .rename(oldPath,newPath)
     .then(() => console.log('Done!'))
-    .catch((console.error));
+    .catch(console.error);
 }
-
